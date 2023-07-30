ad4201acd1fd927a070429e5221f7a3b0b61c53d
Dominik Falkner <dfalkner@Dominiks-MacBook-Pro.local>
2023-07-30 11:11:46 +0200

    Create initial image

---
+++ src/start.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+trap 'kill -TERM $PID' TERM INT
+
+echo "This is Tailscale-Caddy-proxy version"
+cat /VERSION
+
+echo "Building Caddy configfile"
+
+echo $TS_HOSTNAME'.'$TS_TAILNET.'ts.net' > /etc/caddy/Caddyfile
+echo 'reverse_proxy' $CADDY_TARGET >> /etc/caddy/Caddyfile
+
+echo "Starting Caddy"
+caddy start --config /etc/caddy/Caddyfile
+
+echo "Starting Tailscale"
+
+export TS_EXTRA_ARGS=--hostname="${TS_HOSTNAME} ${TS_EXTRA_ARGS}"
+echo "Note: set TS_EXTRA_ARGS to " $TS_EXTRA_ARGS
+/usr/local/bin/containerboot
